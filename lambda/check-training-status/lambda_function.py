4b7b97a48fcc6a3d69bd6091917744c453bd93a7
Dev Pratap Singh <dxs6169@psu.edu>
2025-07-25 12:16:40 +0530

    Update Lambda function code

--- lambda/check-training-status/lambda_function.py
+++ lambda/check-training-status/lambda_function.py
@@ -1,2 +1,12 @@
+import boto3
+
 def lambda_handler(event, context):
-    return {"statusCode": 200, "body": "Hello from Lambda!"}
+    job_name = event["TrainingJobName"]
+    sm_client = boto3.client("sagemaker")
+
+    response = sm_client.describe_training_job(TrainingJobName=job_name)
+    training_status = response["TrainingJobStatus"]
+
+    return {
+        "status": training_status
+    }
