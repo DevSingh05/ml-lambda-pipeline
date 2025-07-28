f01d78fca1ab5abff5f90ea846fded7e2bbc8e25
Dev Pratap Singh <dxs6169@psu.edu>
2025-07-28 11:32:49 +0530

    Add comment to trigger redeploy of check-training-status Lambda

--- lambda/check-training-status/lambda_function.py
+++ lambda/check-training-status/lambda_function.py
@@ -1,6 +1,6 @@
 import boto3
 
-def lambda_handler(event, context):
+def lambda_handler(event, context): # testing a run
     job_name = event["TrainingJobName"]
     sm_client = boto3.client("sagemaker")
 
